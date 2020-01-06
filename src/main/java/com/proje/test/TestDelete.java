aed15f7b7ac5396d6138f91971c0c23a213ff407
KadriyeOner-lab <58609327+KadriyeOner-lab@users.noreply.github.com>
2020-01-06 11:20:52 +0300

    kadriyee

---
+++ src/main/java/com/proje/test/TestDelete.java
@@ -0,0 +1,19 @@
+package com.proje.test;
+
+import com.proje.model.Book;
+import com.proje.reporsitory.BookRepository;
+import com.proje.repository.impl.BookRepositoryImpl;
+
+public class TestDelete {
+
+public static void main(String[] args) {
+		
+		
+		BookRepository bookRepository = new BookRepositoryImpl();
+		
+		Book book = bookRepository.find(100);
+		
+		bookRepository.delete(book);
+		
+	}
+}
---
+++ src/main/java/com/proje/test/TestFind.java
@@ -0,0 +1,26 @@
+package com.proje.test;
+
+import com.proje.model.Book;
+import com.proje.reporsitory.BookRepository;
+import com.proje.repository.impl.BookRepositoryImpl;
+
+public class TestFind {
+	
+	public static void main(String[] args) {
+		
+		
+		BookRepository bookRepository = new BookRepositoryImpl();
+		
+		Book book = bookRepository.find(101);
+		if (book!=null) {
+			
+			System.out.println(book);
+			
+		}
+		else
+			System.out.println("Ürün bulunamadý..");
+		
+		
+	}
+
+}
